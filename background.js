94b00ac68184d8d2e43f3b29dce48705a063793a
Jeff Martin <jmartin@parallax.com>
2017-06-17 21:54:05 -0700

    Enhanced background.js to register a window.onClosed listener that closes all of the app's active serial connections upon application termination.

--- background.js
+++ background.js
@@ -1,11 +1,24 @@
+// Register listeners to create app window upon application launch and
+// to close active serial ports upon application termination
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    id: "BlocklyProp-Launcher",
-    innerBounds: {
-      maxWidth: 500,
-      maxHeight: 500,
-      minWidth: 200,
-      minHeight: 200
-    }, state: "normal"
+    chrome.app.window.create('index.html', {
+        id: "BlocklyProp-Launcher",
+        innerBounds: {
+            maxWidth: 500,
+            maxHeight: 500,
+            minWidth: 200,
+            minHeight: 200
+        }, state: "normal"
+    }, function(win) {
+      win.onClosed.addListener(closeSerialPorts);
+    });
   });
-});
+
+function closeSerialPorts(){
+// Close this app's active serial ports
+    chrome.serial.getConnections(function(activeConnections) {
+        activeConnections.forEach(function(port) {
+            chrome.serial.disconnect(port.connectionId, function() {});
+        });
+    });
+}
