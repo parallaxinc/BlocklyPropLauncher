de7be5346e176c31b92400ef321279c4ec1e3b54
Jeff Martin <jmartin@parallax.com>
2017-06-26 10:59:22 -0700

    Bumped version up to v0.7.0 to match JSON-reported version.  Bumped requried Chrome version up to v45 to a known-stable serial support level.

--- parser.js
+++ parser.js
@@ -1,3 +1,19 @@
+/* Parallax Inc. ("PARALLAX") CONFIDENTIAL
+ Unpublished Copyright (c) 2017 Parallax Inc., All Rights Reserved.
+
+ NOTICE:  All information contained herein is, and remains the property of PARALLAX.  The intellectual and technical concepts contained
+ herein are proprietary to PARALLAX and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade
+ secret or copyright law.  Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
+ permission is obtained from PARALLAX.  Access to the source code contained herein is hereby forbidden to anyone except current PARALLAX
+ employees, managers or contractors who have executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+
+ The copyright notice above does not evidence any actual or intended publication or disclosure of this source code, which includes
+ information that is confidential and/or proprietary, and is a trade secret, of PARALLAX.  ANY REPRODUCTION, MODIFICATION, DISTRIBUTION,
+ PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF PARALLAX IS STRICTLY
+ PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR
+ RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR
+ SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.                                                                                */
+
 
 function parseFile(payload) {
 /* Parse payload (.elf, .binary, or .eeprom format) for Propeller Application image.
