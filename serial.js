f586c29962a1fbc4eb556ac6b030d578dd762c78
Jeff Martin <jmartin@parallax.com>
2018-02-12 16:19:34 -0800

    Added 1/2 second Rx hardware-to-OS slack time in mblDeliveryTime timeout in order to wait long enough for slow reception of expected response.

--- serial.js
+++ serial.js
@@ -3183,11 +3183,11 @@ function talkToProp(sock, cid, binImage, toEEPROM) {
         var next;
 
         /* Calculate expected Micro Boot Loader and User Application delivery times
-           = 300 [>max post-reset-delay] + ((10 [bits per byte] * (data bytes [transmitting] + 20 silence bytes [MBL waiting] +
-             8 MBL "ready" bytes [MBL responding])) / initial baud rate) * 1,000 [to scale ms to integer] + 1 [to always round up] */
-        var mblDeliveryTime = 300+((10*(txData.byteLength+20+8))/initialBaudrate)*1000+1;
+           = 300 [>max post-reset-delay] + ((10 [bits per byte] * (data bytes [transmitting] + 20 silence bytes [MBL waiting] + 8 MBL "ready" bytes [MBL responding])) /
+             initial baud rate) * 1,000 [to scale ms to integer] + 1 [to always round up]  + 500 [Rx hardware to OS slack time] */
+        var mblDeliveryTime = 300+((10*(txData.byteLength+20+8))/initialBaudrate)*1000+1+500;
 
-        //=((10 [bits per byte] * [max packet size]) / final baud rate) * 1,000 [to scale ms to integer] + 1 [to always round up] + 750 [Rx hardware to OS slack time]
+        //=((10 [bits per byte] * [max packet size]) / final baud rate) * 1,000 [to scale ms to integer] + 1 [to always round up] + 500 [Rx hardware to OS slack time]
         var userDeliveryTime = ((10*maxDataSize)/finalBaudrate)*1000+1+500;
 
         //Set for limited retry attemps
