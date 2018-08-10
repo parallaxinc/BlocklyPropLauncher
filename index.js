f2ed360f726f90d07f34f10e84b99b7f2b1b3682
Jeff Martin <jmartin@parallax.com>
2018-08-09 21:04:26 -0700

    Updated sendPortList to use WX's name if not empty, or id, for port list.

--- index.js
+++ index.js
@@ -425,7 +425,10 @@ function sendPortList() {
       });
       //TODO convert to .forEach?
       for(v = 0; v < wx_modules.length; v++) {
-        pt.push(wx_modules[v].name.substr(0,32));
+        // get module name without leading/trailing whitespace
+        var name = wx_modules[v].name.substr(0,32).replace(/(^\s+|\s+$)/g,'');
+        // add name (if not empty) or id
+        pt.push(name.length !== 0 ? name : wx_modules[v].id);
       }
       var msg_to_send = {type:'port-list',ports:pt};
       for (var i = 0; i < sockets.length; i++) {
