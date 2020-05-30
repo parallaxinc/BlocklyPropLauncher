9d7c01df643e4a01257fb03abc2e0882134f0175
Jeff Martin <jmartin@parallax.com>
2020-05-30 09:10:20 -0700

    Partial implementation of socket-specific preferred port setting and send-on-pref-port message.

--- index.js
+++ index.js
@@ -322,9 +322,12 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(connect, 500);
 });
 
-function updatePreferredPort(port) {
-// Remember new preferred port (if not null)
-    if (port && port !== preferredPort.name) {
+function updatePreferredPort(port, socket) {
+/* Remember new preferred port (if not null).
+   Returns true if port is new; false otherwise.
+   socket is the sender associated with this request. */
+    let isNewPort = (port) && (port !== preferredPort.name);
+    if (isNewPort) {
         preferredPort.name = port;
         if (chrome.storage) {
             chrome.storage.sync.set({'pref_port': preferredPort.name}, function () {if (chrome.runtime.lastError) {storageError()}});
@@ -332,6 +335,7 @@ function updatePreferredPort(port) {
         // A "new" port selection was made; set all existing ports to non-new status
         clearNewPortStatus();
     }
+    return isNewPort;
 }
 
 function sm32bit() {
@@ -404,16 +408,16 @@ function connect_ws(ws_port, url_path) {
                 if (ws_msg.type === "load-prop") {
                     // load the propeller
                     log('Received Propeller Application for ' + ws_msg.action, mDbug, socket, 1);
-                    updatePreferredPort(ws_msg.portPath);
+                    updatePreferredPort(ws_msg.portPath, socket);
                     setTimeout(function() {loadPropeller(socket, ws_msg.portPath, ws_msg.action, ws_msg.payload, ws_msg.debug)}, 10);  // success is a JSON that the browser generates and expects back to know if the load was successful or not
                 } else if (ws_msg.type === "serial-terminal") {
                     // open or close the serial port for terminal/debug
-                    updatePreferredPort(ws_msg.portPath);
+                    updatePreferredPort(ws_msg.portPath, socket);
                     serialTerminal(socket, ws_msg.action, ws_msg.portPath, ws_msg.baudrate, ws_msg.msg); // action is "open", "close" or "msg"
                 } else if (ws_msg.type === "pref-port") {
                     // user selected a new preferred port
-                    updatePreferredPort(ws_msg.portPath);
                     log('User selected preferred port: ' + ws_msg.portPath, mDbug, socket, 1);
+                    updatePreferredPort(ws_msg.portPath, socket);
                 } else if (ws_msg.type === "port-list-request") {
                     // send an updated port list (and continue on scheduled interval)
                     log('Site requested port list', mDbug, socket, 1);
@@ -451,9 +455,9 @@ function connect_ws(ws_port, url_path) {
 // Port Lister management functions
 // The Port Lister items are timers (and sockets) to automatically send wired/wireless port updates to connected browser sockets
 function addPortLister(socket) {
-//Create new port lister (to send port lists to browser on a timed interval).
+//Create new port lister (to send port lists to browser on a timed interval) using last saved preferred port as the default.
 //socket is the browser socket to send updates to.
-    startPortListerScanner(portLister.push({socket: socket})-1);
+    startPortListerScanner(portLister.push({socket: socket, prefPort: preferredPort})-1);
 }
 
 function startPortListerScanner(idx) {
@@ -580,10 +584,10 @@ function scanWXPorts() {
 }
 
 function sendPortList(socket) {
-/* Send current list of communication ports to browser via socket.
+/* Send list of current communication ports to browser via socket.
    (See "Launcher Communication Port Rules," above, for detailed rules and scenarios this function (and ports.js and index.js) implements.)
-   List is ordered as: blank (rarely) or preferred (if any) followed by sorted... new wired, old wired, new wireless, then old wireless ports.
-   New means newly-arrived (since last port selection changed); old means existing since before last port selection changed.*/
+   List is ordered as: blank (rarely) or preferred (if any) followed by individually sorted groups of new wired, old wired, new wireless, then old wireless ports.
+   "New" means newly-arrived (since last port selection changed); "old" means existing since before last port selection changed.*/
     let bp = [];      // Either empty (common) or blank string (rarely)
     let pp = [];      // Peferred port name (qty 0 or 1)
     let nwp = [];     // New wired port name list (often qty 0 or 1)
@@ -591,35 +595,39 @@ function sendPortList(socket) {
     let nwlp = [];    // New wireless port name list (=> 0)
     let owlp = [];    // Old Wireless port (=> 0)
     let qty = 0;      // Quantity of ports found
+    let lister = portLister.find(function(p) {return p.socket === socket});  //Find the portLister object that belongs to this socket
 
-    // gather separated port lists (preferred port (if any), new wired/wireless ports (if any), old wired/wireless ports, then sort them)
-    ports.forEach(function(p) {
-        if (p.name === preferredPort.name) {
-            pp.push(p.name)
-        } else {
-            if (p.isWired) {
-                if (p.new) {nwp.push(p.name)} else {owp.push(p.name)}
+    if (lister) {
+        // Found our required lister object
+        // gather separated port lists (preferred port (if any), new wired/wireless ports (if any), old wired/wireless ports, then sort them)
+        ports.forEach(function(p) {
+            if (p.name === lister.prefPort.name) {
+                pp.push(p.name)
             } else {
-                if (p.new) {nwlp.push(p.name)} else {owlp.push(p.name)}
+                if (p.isWired) {
+                    if (p.new) {nwp.push(p.name)} else {owp.push(p.name)}
+                } else {
+                    if (p.new) {nwlp.push(p.name)} else {owlp.push(p.name)}
+                }
             }
-        }
-    });
-    nwp.sort();
-    owp.sort();
-    nwlp.sort();
-    owlp.sort();
-    qty = pp.length+nwp.length+owp.length+nwlp.length+owlp.length;
+        });
+        nwp.sort();
+        owp.sort();
+        nwlp.sort();
+        owlp.sort();
+        qty = pp.length+nwp.length+owp.length+nwlp.length+owlp.length;
 
-    // Remember when the preferredPort exists; otherwise if preferredPort just disappeared, clear all "new" port statuses - we only care about new-arrivals since last preferred port selection
-    if (pp.length) {preferredPort.exists = true} else {if (preferredPort.exists) {preferredPort.exists = false; clearNewPortStatus();}}
+        // Remember when the preferredPort exists; otherwise if preferredPort just disappeared, clear all "new" port statuses - we only care about new-arrivals since last preferred port selection
+        if (pp.length) {preferredPort.exists = true} else {if (preferredPort.exists) {preferredPort.exists = false; clearNewPortStatus();}}
 
-    // report back to editor; blank (rarely), preferred port first (if any), new wired ports (if any), old wired ports, new wireless ports (if any), and finally old wireless ports
-    if (qty && !pp.length && !nwp.length) {bp.push("")}  // Send leading blank port only if > 0 ports found, none match the preferred port, and there are no new wired ports
-    var msg_to_send = {type:'port-list',ports:bp.concat(pp.concat(nwp.concat(owp.concat(nwlp.concat(owlp)))))};
+        // report back to editor; blank (rarely), preferred port first (if any), new wired ports (if any), old wired ports, new wireless ports (if any), and finally old wireless ports
+        if (qty && !pp.length && !nwp.length) {bp.push("")}  // Send leading blank port only if > 0 ports found, none match the preferred port, and there are no new wired ports
+        var msg_to_send = {type:'port-list',ports:bp.concat(pp.concat(nwp.concat(owp.concat(nwlp.concat(owlp)))))};
 
-    log('Sending port list' + (!verboseLogging ? ' (qty '+qty+')' : ': ' + msg_to_send.ports.toString()), mDbug, socket, -1);
-    socket.send(JSON.stringify(msg_to_send));
-    if (chrome.runtime.lastError) {log(chrome.runtime.lastError, mDbug)}
+        log('Sending port list' + (!verboseLogging ? ' (qty '+qty+')' : ': ' + msg_to_send.ports.toString()), mDbug, lister.socket, -1);
+        lister.socket.send(JSON.stringify(msg_to_send));
+        if (chrome.runtime.lastError) {log(chrome.runtime.lastError, mDbug)}
+    }
 }
 
 
